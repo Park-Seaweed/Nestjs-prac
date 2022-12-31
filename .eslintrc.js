97c824dc69d379a67e350a2fc66ba4412da1295e
Park-Seaweed <qkralsgur721@gmail.com>
2022-12-31 19:20:17 +0900

    add: Board모듈 생성

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// 특정한 규칙을 가지고 코드를 깔끔하게 짤수있게 도와주는 라이브러리
+// 타입스크립트를 쓰는 가이드 라인제시, 문법에 오류가나면 알려주는 역할 등등
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardService: BoardsService) {}
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
+  }
+}
